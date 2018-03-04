d739106cbb48b87b9361f3c0d0cb5aeb7386775b
ragsdalejohn <john.ragsdale@umontana.edu>
2018-03-04 10:13:20 -0700

    rev 16

--- course-work/HW-4/sketch.js
+++ course-work/HW-4/sketch.js
@@ -3,13 +3,13 @@ function setup() {
 
 // create canvas
 createCanvas( 500, 500 );
+background( "brown" );
 
 }
 
 function draw() {
-  // background color
-  background( "blue" );
-
-
+  // draw a dot
+  strokeWeight(10);
+  point( 100, 50 )
 
 }
