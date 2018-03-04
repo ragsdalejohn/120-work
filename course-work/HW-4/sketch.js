879ce3e915c6b777abfe6ee7069ff25cc270b909
ragsdalejohn <john.ragsdale@umontana.edu>
2018-03-04 10:21:41 -0700

    rev 18

--- course-work/HW-4/sketch.js
+++ course-work/HW-4/sketch.js
@@ -9,12 +9,8 @@ background( "brown" );
 
 function draw() {
   // draw a dot
-  strokeWeight(10);
+  strokeWeight(20);
   point( 100, 50 );
   point( 250, 50 );
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> bb2d724adf459eb0aca87c917271bd9d3b515de0
 }
