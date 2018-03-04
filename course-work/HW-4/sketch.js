cc6ebf54170a5d3af49d0848da6c0ee9d93f3764
ragsdalejohn <john.ragsdale@umontana.edu>
2018-03-04 09:26:57 -0700

    rev 10

--- course-work/HW-4/sketch.js
+++ course-work/HW-4/sketch.js
@@ -20,8 +20,6 @@ function draw() {
 
 
   // Nose
-    triangle( 50, 50, 250, 50, 150, 180 );
-    triangle( 350, 50, 550, 50, 450, 180 );
-    triangle( 10, 225, 590, 225, 375, 375 );
-
+    strokeweight( 30 );
+    point( 0, 0 ); 
 }
