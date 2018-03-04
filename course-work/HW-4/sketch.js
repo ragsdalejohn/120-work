2a61d32662097a140f4a4348259dcc05146e8755
ragsdalejohn <john.ragsdale@umontana.edu>
2018-03-04 12:53:55 -0700

    rev 28

--- course-work/HW-4/sketch.js
+++ course-work/HW-4/sketch.js
@@ -12,11 +12,9 @@ function draw() {
   // draw head - hope is lantern shaped
   fill('orange');
   ellipse( 250, 250, 300, 150);
-}
 
-function draw() {
   // draw dots for eyes
-  strokeWeight(50)
+  strokeWeight(50);
   point( 100, 50 );
   point( 400, 50 );
   point( 250, 250 );
