89d3d5adc76c88ce89db5feeda040fd23fdf3375
ragsdalejohn <john.ragsdale@umontana.edu>
2018-03-04 08:46:46 -0700

    HW 4 begin

---
+++ course-work/HW-4/sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+// setup up this assignment
+
+// create canvas
+createCanvas( 600, 900 );
+
+}
+
+function draw() {
+  // background color
+  background( "black" )
+
+  // ********
+  // Create sandbox
+  push();
+
+  // move to center of canvas
+  translate( 300,450 );
+
+  // HEAD
+  push();
+  translate( 0,-175 );
+
+  fill( "orange" );
+  ellipse( 0, 0, 330, 200 );
+
+  // Nose
+  fill( "black" );
+  Triangle ( 0, -20, 30, 20, -30, 20 );
+
+  pop();
+
+  pop();
+}
