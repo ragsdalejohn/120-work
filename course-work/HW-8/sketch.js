00ae0dccecd45f957286d4c766adf9d9b1fe65d9
ragsdalejohn <john.ragsdale@umontana.edu>
2018-04-08 11:45:14 -0600

    Rev 3

--- course-work/HW-8/sketch.js
+++ course-work/HW-8/sketch.js
@@ -1,8 +1,12 @@
 
 // The original author: Michael Musick <michaelmusick>
 // I have modified this code from its original
-// 
+// I will try to explain the syntax of the code and
+// what is happening.
 
+
+// set up the canvas area to the width of the window
+// height of 300
 
 function setup() {
     createCanvas(windowWidth, 300);
@@ -10,10 +14,16 @@ function setup() {
 
 
 // using 'let' instead of 'var'
+// using let allows variable to be "locked"
 let pos_x = 0;
+// popsition of X starts where x=0
 let pos_y = 0;
+// position of Y starts where y=0
 let d = 30;
+// diameter is = to 30 pixels
 let r = d / 2;
+// radius is = to diameter / 2
+
 
 function draw() {
     background('rgb(3, 78, 115)');
