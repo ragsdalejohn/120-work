045995eb842a795e1c77ed0bfecd8f2ca02e2219
ragsdalejohn <john.ragsdale@umontana.edu>
2018-04-09 16:24:50 -0600

    New HW 10

---
+++ course-work/HW-10/readme.md
@@ -0,0 +1,3 @@
+John Ragsdale, 50
+
+Knowing as little as I do about syntax in this p5.js enviornment, I fumbled and jumbled this together. Not necessarily meant to be crass or rude, however, it is frustrating when you have a concept in your head and translating that into code is more challenging.
---
+++ course-work/HW-10/sketch.js
@@ -0,0 +1,37 @@
+function setup(){
+    createCanvas( windowWidth, 400 );
+    frameRate(1);
+}
+
+function draw() {
+    background( 'rgb(91, 255, 147)' );
+
+    // create numbers to pass our function
+    let num1 = floor(random(0, 50));
+    let num2 = floor(random(0, 50));
+    let r = result;
+    // call our newly defined function
+    addThings( num1, num2 );
+    whatInTheAss( result );
+}
+
+// Our newly defined function
+function addThings( value1, value2 ) {
+    // 1. Add the values together. Store them into some variable.
+    let result = value1 + value2;
+
+    // 2. Print the results as a string to the canvas.
+    result = "The result of adding '" + value1 + "' and '" + value2 + "' is '" + result + "'.";
+    textSize(18);
+    textAlign( CENTER );
+    text( result, width/2, height/2 );
+
+function whatInTheAss( r ) {
+    let r = result
+    if r > 50 = "What in the ass???"
+    textSize(36);
+    textAlign( BOTTOM );
+
+
+}
+}
