73a5010b09eb3de1b814cdf4ca4b728b7c13ddc5
ragsdalejohn <john.ragsdale@umontana.edu>
2019-04-24 12:14:44 -0600

    rev 15

--- course-work/HW-10/readme.md
+++ course-work/HW-10/readme.md
@@ -1,3 +1,6 @@
 John Ragsdale, 50
 
-Knowing as little as I do about syntax in this p5.js enviornment, I fumbled and jumbled this together. Not necessarily meant to be crass or rude, however, it is frustrating when you have a concept in your head and translating that into code is more challenging. I have been getting help From Emily on this. 
+Knowing as little as I do about syntax in this p5.js enviornment, I fumbled and jumbled this together. Not necessarily meant to be crass or rude, however, it is frustrating when you have a concept in your head and translating that into code is more challenging. I have been getting help From Emily on this.
+
+This function is supposed to call a simple senetnce, give 2 variables, add them
+and produce as result. If the result is greater than 50, then answer is supposed to say "What in the ass?"
--- course-work/HW-10/sketch.js
+++ course-work/HW-10/sketch.js
@@ -1,27 +1,29 @@
 function setup(){
     createCanvas( windowWidth, 400 );
+    // refreshing at a certain framerate
     frameRate(1);
 }
 
 function draw() {
     background( 'rgb(91, 255, 147)' );
 
-    // create numbers to pass our function
+    // create random numbers for the values
     let value1 = floor(random(0, 50));
     let value2 = floor(random(0, 50));
+//tried var and let to get result
     let result = value1 + value2;
     // call our newly defined function
     addThings( value1, value2 );
 
 }
 
-// Our newly defined function
+//new defined function
 function addThings( value1, value2 ) {
-    // 1. Add the values together. Store them into some variable.
-    //let result = value1 + value2;
+    // Add the values together and store as result
+    // let result = value1 + value2;
 
-    // 2. Print the results as a string to the canvas.
-    //result = text("The result of adding '" + value1 + "' and '" + value2 + "' is '" + result + "'.");
+    // Display values and result
+    // result = text("The result of adding '" + value1 + "' and '" + value2 + "' is '" + result + "'.");
 
     text("The result of adding '" + value1 + "' and '" + value2 + "' is '" + result + "'.", 40, 40);
     textSize(18);
@@ -29,6 +31,8 @@ function addThings( value1, value2 ) {
     text( result, width/2, height/2 );
 
 
+// Emily modified this function
+// this function is for if the result is greater than 50
 function whatInTheAss( result ) {
     if (result > 50) {
       text("What in the ass?", 50,50);
@@ -36,6 +40,7 @@ function whatInTheAss( result ) {
       text(result, 50,50);
     }
 
+// tried this - didnt work
     //if (result > 50){ == "What in the ass???"
     textSize(36);
     textAlign( BOTTOM );
