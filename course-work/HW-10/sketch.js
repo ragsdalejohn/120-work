16e4c854e40e6f3e4b946aeb16094bf9f8e6d727
ragsdalejohn <john.ragsdale@umontana.edu>
2019-04-24 11:51:28 -0600

    Update course-work/HW-10/sketch.js
    rev 10

--- course-work/HW-10/sketch.js
+++ course-work/HW-10/sketch.js
@@ -22,6 +22,7 @@ function addThings( value1, value2 ) {
 
     // 2. Print the results as a string to the canvas.
     //result = "The result of adding '" + value1 + "' and '" + value2 + "' is '" + result + "'.";
+
     text("The result of adding '" + value1 + "' and '" + value2 + "' is '" + result + "'.", 40, 40);
     textSize(18);
     textAlign( CENTER );
